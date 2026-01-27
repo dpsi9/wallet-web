38a8bdf17992f0ff55244e49c697e2efdfe05e2e
Dipendra Singh <dipendras953@gmail.com>
2026-01-28 01:48:45 +0530

    add token filter

---
+++ chain/getMintInfo.ts
@@ -0,0 +1,16 @@
+import { fetchMint } from "@solana-program/token-2022";
+import { rpc } from "@/chain/solana";
+import { Address } from "@solana/kit";
+
+const MINT_CACHE = new Map<string, any>();
+
+export async function getMintInfo(mint: string) {
+  if (MINT_CACHE.has(mint)) return MINT_CACHE.get(mint);
+
+  const mintPk = mint as Address;
+  const mintInfo = await fetchMint(rpc, mintPk);
+
+  MINT_CACHE.set(mint, mintInfo);
+
+  return mintInfo;
+}
---
+++ chain/tokenFilter.ts
@@ -0,0 +1,35 @@
+import { getMintInfo } from "./getMintInfo";
+
+export async function classifyToken(asset: any) {
+  if (asset.interface !== "FungibleToken") return null;
+
+  const info = asset.token_info;
+  const meta = asset.content?.metadata;
+
+  if (!info || !meta?.name || !meta?.symbol) return null;
+  if (info.decimals > 18) return null;
+
+  const balance = info.balance / 10 ** info.decimals;
+  if (balance < 0.0001) return null;
+
+  const text = `${meta.name} ${meta.symbol}`.toLowerCase();
+  const banned = ["http", "visit", "claim", "airdrop", "free"];
+
+  if (banned.some((w) => text.includes(w))) return null;
+
+  const mintInfo = await getMintInfo(asset.id);
+
+  const hasMintAuthority = mintInfo.mintAuthority !== null;
+  const hasFreezeAuthority = mintInfo.freezeAuthority != null;
+
+  const trusted = !hasFreezeAuthority && !hasMintAuthority;
+
+  return {
+    mint: asset.id,
+    name: meta.name,
+    symbol: meta.symbol,
+    balance,
+    decimals: info.decimals,
+    trusted,
+  };
+}
